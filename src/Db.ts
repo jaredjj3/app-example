856c15d30cec82bdae84a9632661945d8993c199
Jared Johnson <jared@jaredjohnson.dev>
2021-08-07 21:08:51 -0400

    Create Db wrapper for MikroORM

---
+++ src/Db.ts
@@ -0,0 +1,45 @@
+import { MikroORM, UnderscoreNamingStrategy } from 'mikro-orm';
+
+export class Db {
+  private didInit = false;
+  private ormRef: MikroORM;
+
+  async init() {
+    if (this.didInit) {
+      return;
+    }
+    this.ormRef = await MikroORM.init({
+      type: 'postgresql',
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT),
+      dbName: process.env.DB_NAME,
+      user: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      namingStrategy: UnderscoreNamingStrategy,
+      entities: [],
+      discovery: {
+        requireEntitiesArray: false,
+        warnWhenNoEntities: false,
+      },
+    });
+    this.didInit = true;
+  }
+
+  get orm(): MikroORM {
+    if (typeof this.ormRef === 'undefined') {
+      throw new Error('must call Db.init before accessing orm');
+    }
+    return this.ormRef;
+  }
+
+  async close() {
+    if (this.didInit) {
+      await this.orm.close(true);
+    }
+  }
+
+  async cleanup() {
+    // TODO(jared) Add entities to delete here.
+    this.orm.em.clear();
+  }
+}
