eb1fa1387ecf785da162f14a2b79e7c6389c993a
Jared Johnson <jared@jaredjohnson.dev>
2021-08-08 08:06:22 -0400

    Run database migration before test

--- src/migrations/Migration20210808014510.ts
+++ src/migrations/Migration20210808014510.ts
@@ -2,6 +2,29 @@ import { Migration } from '@mikro-orm/migrations';
 
 export class Migration20210808014510 extends Migration {
   async up(): Promise<void> {
-    this.addSql('select 1');
+    await this.execute(`
+CREATE TABLE users (
+  id SERIAL PRIMARY KEY,
+  username text UNIQUE NOT NULL
+);
+
+CREATE TABLE posts (
+  id SERIAL PRIMARY KEY,
+  title TEXT NOT NULL,
+  body TEXT NOT NULL,
+  author_id integer REFERENCES users (id)
+);
+
+CREATE TABLE tags (
+  id SERIAL PRIMARY KEY,
+  name TEXT UNIQUE NOT NULL
+);
+
+CREATE TABLE post_tags (
+  id SERIAL PRIMARY KEY,
+  post_id integer REFERENCES posts (id),
+  tag_id integer REFERENCES tags (id)
+);
+  `);
   }
 }
